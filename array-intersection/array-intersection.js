6dbc1770192512c9d5941c423a2559dea72324cd
Ahmed WAeer <ahmedwaeer@gmail.com>
2021-08-10 14:27:20 +0200

    A function that accepts two arrays of numbers Return a new array containing the intersecting elements of the arrays

--- array-intersection/array-intersection.js
+++ array-intersection/array-intersection.js
@@ -1,5 +1,4 @@
-// Write code to create a function that accepts two arrays of numbers
-// Return a new array containing the intersecting elements of the arrays
+// Write code to create A function that accepts two arrays of numbers Return a new array containing the intersecting elements of the arrays
 
 var arrayIntersection = function(arr1, arr2) {
 
